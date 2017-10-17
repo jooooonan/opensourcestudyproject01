54a4662a58c7e4759fbad0dd13161cc70665779f
jooooonan <wnsgkstm@gmail.com>
2017-10-17 20:13:56 +0900

    첫번째 커밋

---
+++ app/src/main/java/com/example/wnsgk/opensourcetest01/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.wnsgk.opensourcetest01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.opensourcestudylib.TestPrint;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TestPrint testPrint = new TestPrint();
+
+        
+
+    }
+}
---
+++ opensourcestudylib/src/main/java/com/example/opensourcestudylib/TestPrint.java
@@ -0,0 +1,11 @@
+package com.example.opensourcestudylib;
+
+/**
+ * Created by wnsgk on 2017-10-17.
+ */
+
+public class TestPrint {
+
+
+
+}
