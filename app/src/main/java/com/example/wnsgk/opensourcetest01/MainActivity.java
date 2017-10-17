2c2b963072c5d55ff1a9f0f14134a5af51f8dc80
jooooonan <wnsgkstm@gmail.com>
2017-10-17 20:49:25 +0900

    빌드실패로 인한 재 릴리즈...

--- app/src/main/java/com/example/wnsgk/opensourcetest01/MainActivity.java
+++ app/src/main/java/com/example/wnsgk/opensourcetest01/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.wnsgk.opensourcetest01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.opensourcestudylib.TestPrint;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TestPrint testPrint = new TestPrint();
 
 
 
